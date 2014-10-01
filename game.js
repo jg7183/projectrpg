0f16c8ac2cd8fcc41a02d67071b655bc75022612
jg7183 <jg7183@yahoo.com.au>
2014-10-01 14:01:04 +1000

    Added initial files
    Lets do this!

---
+++ game.js
@@ -0,0 +1,18 @@
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+
+function preload() {
+
+//In this function you preload the game assets
+
+}
+
+function create() {
+//Display the preloaded game assets on the screen
+
+    
+}
+
+function update(){
+//Will constantly be looped over checking for changes, i.e the up button has been pressed	
+	
+	};
